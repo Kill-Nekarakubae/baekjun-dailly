9e817d99b042969ebadae40f9a2ff1f2fdef94ad
Goban <gobanest@gmail.com>
2023-07-13 00:56:58 +0900

    [goban] 데일리 알고리즘

--- Goban/SwiftAlgorithm/BOJ/DailyPicked/0712/적록색약/main.swift
+++ Goban/SwiftAlgorithm/BOJ/DailyPicked/0712/적록색약/main.swift
@@ -11,10 +11,65 @@ import Foundation
 // 적록색약 board를 만들어 R을 G 로 변경
 
 var n = Int(readLine()!)!
-var board: [[String]] = []
-for i in 0..<n {
-    let line = String(readLine()!)
-    board[i].append(line)
+var normalVisited: [[Bool]] = Array(repeating: Array(repeating: false, count: n), count: n)
+var redVisited: [[Bool]] = Array(repeating: Array(repeating: false, count: n), count: n)
+var normalBoard: [[String]] = []
+let dy = [-1, 1, 0, 0]
+let dx = [0, 0, -1, 1]
+
+for _ in 0..<n {
+    let line = String(readLine()!).map{ String($0) }
+    normalBoard.append(line)
 }
 var redBoard: [[String]] = []
+for i in 0..<n {
+    var line: [String] = []
+    for j in 0..<n {
+        var color = normalBoard[i][j]
+        if color == "R" {
+            color = "G"
+        }
+        line.append(color)
+    }
+    redBoard.append(line)
+}
 
+var stack: [(x: Int, y: Int)] = []
+func BFS(color: String, red: Bool) {
+    while !stack.isEmpty {
+        let (x, y) = stack.popLast()!
+        for i in 0...3 {
+            let mx = x + dx[i]
+            let my = y + dy[i]
+            if 0..<n ~= mx && 0..<n ~= my && ( red ? !redVisited[my][mx] : !normalVisited[my][mx]) && (red ? color == redBoard[my][mx] : color == normalBoard[my][mx] ) {
+                stack.append((x: mx, y: my))
+                if red {
+                    redVisited[my][mx] = true
+                } else {
+                    normalVisited[my][mx] = true
+                }
+            }
+        }
+    }
+}
+
+var normalCount = 0
+var redCount = 0
+for i in 0..<n {
+    for j in 0..<n {
+        if normalVisited[i][j] == false {
+            stack.append((x: j, y: i))
+            normalVisited[i][j] = true
+            BFS(color: normalBoard[i][j], red: false)
+            normalCount += 1
+        }
+        if redVisited[i][j] == false {
+            stack.append((x: j, y: i))
+            redVisited[i][j] = true
+            BFS(color: redBoard[i][j], red: true)
+            redCount += 1
+        }
+    }
+}
+
+print(normalCount, redCount)
