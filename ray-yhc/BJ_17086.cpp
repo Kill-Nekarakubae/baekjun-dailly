f299d9bb609366f004a7ebcd0858a4f94e8e214b
ray-yhc <raycho521@yonsei.ac.kr>
2023-08-26 19:13:41 +0900

    feat: study

--- ray-yhc/BJ_17086.cpp
+++ ray-yhc/BJ_17086.cpp
@@ -13,20 +13,6 @@ int smap[1001][1001];
 char cho[1001];
 
 int main() {
-    int N, M;
-
-    fill(smap[0], smap[1001], -1);
-
-    for (int i = 0; i < N; ++i) {
-        cin >> cho[i];
-    }
-    for (int i = 0; i < M; ++i) {
-        int a, b, d;
-        cin >> a >> b >> d;
-        if (cho[a]==cho[b]) continue;
-        smap[a][b] = smap[b][a] = d;
-    }
-
 
     return 0;
 }
