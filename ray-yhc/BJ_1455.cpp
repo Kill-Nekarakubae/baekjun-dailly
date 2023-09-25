d7f6234f5bf6a26bcc8bf3729ac702b181f72771
ray-yhc <raycho521@yonsei.ac.kr>
2023-09-25 18:44:35 +0900

    feat: study

---
+++ ray-yhc/BJ_1455.cpp
@@ -0,0 +1,27 @@
+//
+// Created by 조윤호 on 2023/07/12.
+//
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N, M;
+    int mapp[101][101];
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            cin >> mapp[i][j];
+        }
+    }
+
+    int now = 0;
+
+
+
+
+    return 0;
+}
