d10952497fc8d51f1bdd3fbaabc9467382e093ed
ray-yhc <raycho521@yonsei.ac.kr>
2023-10-03 23:11:44 +0900

    feat: study

--- ray-yhc/BJ_13460.cpp
+++ ray-yhc/BJ_13460.cpp
@@ -16,54 +16,107 @@ int N, M;
 int dr[4] = {-1, 0, 0, 1};
 int dc[4] = {0, -1, 1, 0};
 
-pi blue, red;
+bool redFirst(int direction, pi blue, pi red) {
+    bool redIsUp = (red.first <= blue.first);
+    bool redIsLeft = (red.second <= blue.second);
 
-bool redFirst(int direction) {
-    return false;
+    switch (direction) {
+        case 0: return redIsUp;
+        case 1 : return redIsLeft;
+        case 2: return !redIsLeft;
+        case 3 : return !redIsUp;
+    }
 }
 
-int dfs(vector<vector<int> > visited) {
+int dfs(vector<vector<int> > visited, pi blue, pi red) {
+    if (mapp[blue.first][blue.second] == 'O') return 0;
+    if (mapp[red.first][red.second] == 'O') return 1;
 
-
+    int minSteps = 100000;
     for (int d = 0; d < 4; ++d) {
-        if (redFirst(d)) {
-            int nr = red.first;
-            int nc = red.second;
+        pi nRed(red);
+        pi nBlue(blue);
+
+        if (redFirst(d, nBlue, nRed)) {
+            int nr = nRed.first;
+            int nc = nRed.second;
             for (int i = 0;; i++) {
                 nr += dr[d];
                 nc += dc[d];
 
-                if (mapp[nr][nc] == '#' || (nr == blue.first && nc == blue.second)) {
-                    red.first = nr - dr[d];
-                    red.second = nc - dc[d];
+                if (mapp[nr][nc] == '#' || (nr == nBlue.first && nc == nBlue.second)) {
+                    nRed.first = nr - dr[d];
+                    nRed.second = nc - dc[d];
                     break;
                 } else if (mapp[nr][nc] == 'O') {
-                    red.first = nr;
-                    red.second = nc;
+                    nRed.first = nr;
+                    nRed.second = nc;
                     break;
                 }
             }
 
-            visited[red.first][red.second]++;
-            if (visited[red.first][red.second] > 2) return 0;
+            visited[nRed.first][nRed.second]++;
+            if (visited[nRed.first][nRed.second] > 2) return 0;
 
+            nr = nBlue.first;
+            nc = nBlue.second;
+            for (int i = 0;; i++) {
+                nr += dr[d];
+                nc += dc[d];
 
-
-
+                if (mapp[nr][nc] == '#' || (nr == nRed.first && nc == nRed.second)) {
+                    nBlue.first = nr - dr[d];
+                    nBlue.second = nc - dc[d];
+                    break;
+                } else if (mapp[nr][nc] == 'O') {
+                    nBlue.first = nr;
+                    nBlue.second = nc;
+                    break;
+                }
+            }
         } else {
+            int nr = nBlue.first;
+            int nc = nBlue.second;
+            for (int i = 0;; i++) {
+                nr += dr[d];
+                nc += dc[d];
 
+                if (mapp[nr][nc] == '#' || (nr == nRed.first && nc == nRed.second)) {
+                    nBlue.first = nr - dr[d];
+                    nBlue.second = nc - dc[d];
+                    break;
+                } else if (mapp[nr][nc] == 'O') {
+                    nBlue.first = nr;
+                    nBlue.second = nc;
+                    break;
+                }
+            }
+
+            nr = nRed.first;
+            nc = nRed.second;
+            for (int i = 0;; i++) {
+                nr += dr[d];
+                nc += dc[d];
+
+                if (mapp[nr][nc] == '#' || (nr == nBlue.first && nc == nBlue.second)) {
+                    nRed.first = nr - dr[d];
+                    nRed.second = nc - dc[d];
+                    break;
+                } else if (mapp[nr][nc] == 'O') {
+                    nRed.first = nr;
+                    nRed.second = nc;
+                    break;
+                }
+            }
+            visited[nRed.first][nRed.second]++;
+            if (visited[nRed.first][nRed.second] > 2) return 0;
         }
-
-
-        if (mapp[blue.first][blue.second] == 'O') {
-            return 0;
-        }
-        if (mapp[red.first][red.second] == 'O') {
-            return 1;
-        }
-
-
+        int steps = dfs(visited, nBlue, nRed);
+        if (steps == 0) continue;
+        minSteps = min(steps, minSteps);
     }
+    if (minSteps == 100000) return 0;
+    return minSteps;
 }
 
 void printmap() {
@@ -77,6 +130,8 @@ void printmap() {
 }
 
 int main() {
+
+    pi blue, red;
     cin >> N>>M ;
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < M; ++j) {
@@ -94,9 +149,9 @@ int main() {
 
     vector<vector<int> > visited(N);
     for (int i = 0; i < N; ++i) {
-        visited.push_back(vector<int>(M, 0));
+        visited[i] = vector<int>(M, 0);
     }
     visited[red.first][red.second] = 1;
-    cout << dfs(visited);
+    cout << dfs(visited, blue, red);
     return 0;
 }
